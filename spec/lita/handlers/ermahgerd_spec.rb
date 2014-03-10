87296e22c5c9520a43309a2b27f9ffe4923e8fc8
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-03-10 10:28:10 -0700

    Update spec

--- spec/lita/handlers/ermahgerd_spec.rb
+++ spec/lita/handlers/ermahgerd_spec.rb
@@ -4,12 +4,12 @@ describe Lita::Handlers::Ermahgerd, lita_handler: true do
 
   let(:text) { rand(36**10).to_s(36) }
 
-  it { doesnt_route("ermahgerd #{text}").to(:ermahgerd) }
-  it { routes_command("ermahgerd #{text}").to(:ermahgerd) }
+  it { routes("ermgd #{text}").to(:ermahgerd) }
+  it { routes_command("ermgd #{text}").to(:ermahgerd) }
 
   it "ermahgerds" do
 
-    send_command("ermahgerd #{text}")
+    send_command("ermgd #{text}")
     expect(replies.last).to eq(::Ermahgerd.translate(text))
   end
 end
